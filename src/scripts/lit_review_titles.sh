e07bae9b4d078c38fe1b87af1ebe968925a68bae
juan-amorocho <jamoroch@gmail.com>
2019-09-13 16:04:30 +0200

    fix random selection algorithm

--- src/scripts/lit_review_titles.sh
+++ src/scripts/lit_review_titles.sh
@@ -11,8 +11,18 @@ NUM_LIT_ENTRIES=$(cat $LIT_FILE | wc -l)
 
 SAMPLE_SIZE=$(($NUM_LIT_ENTRIES / 5))
 
-
+ACC=''
 for i in $(seq $SAMPLE_SIZE); do
   PICK=$(( RANDOM % ($NUM_LIT_ENTRIES + 1) ))
+  ACC+=' '$PICK
+  NUM_DIFFERENT_ENTRIES=$(echo $ACC | tr ' ' '\n' | sort | uniq | wc -l)
+  while [ "$NUM_DIFFERENT_ENTRIES" -eq "$i" ]
+  do
+    PICK=$(( RANDOM % ($NUM_LIT_ENTRIES + 1) ))
+    ACC+=' '$PICK
+    NUM_DIFFERENT_ENTRIES=$(echo $ACC  | tr ' ' '\n' | sort | uniq | wc -l)
+  done
   nl $LIT_FILE | cut -d: -f1 | sed -n ${PICK}p | tee -a $OUTFILE
 done
+
+sort -o $OUTFILE $OUTFILE
