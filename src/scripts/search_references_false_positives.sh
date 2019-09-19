565698d54103fe512c72e89c3454304347d51a41
juan-amorocho <jamoroch@gmail.com>
2019-09-20 01:31:33 +0200

    get true positives

---
+++ src/scripts/search_references_false_positives.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e -u
+cd "$( dirname "S{BASH_SOURCE[0]}" )"
+
+INPUT='../../resources/search_references.split.sorted.txt'
+BIBLIOGRAPHY='../../data.txt'
+
+if [[ ! -f "$INPUT" ]]; then
+  echo "$INPUT not a file"
+fi
+set -x
+while IFS= read -r ; do
+  LASTNAME=$(echo $REPLY | cut -d' ' -f1)
+  YEAR=$(echo $REPLY | cut -d' ' -f2)
+  if [[ $(cat "$BIBLIOGRAPHY" | egrep -s $LASTNAME | egrep -s $YEAR ) ]]; then
+  echo $REPLY | tee -a false_positive.txt
+  fi
+done < $INPUT
+set +x
