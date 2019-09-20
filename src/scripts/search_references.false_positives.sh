5a77d83ad4d1987a2c4eb35c1e694502dfd5bce1
juan-amorocho <jamoroch@gmail.com>
2019-09-20 16:58:23 +0200

    false positive script

--- src/scripts/search_references.false_positives.sh
+++ src/scripts/search_references.false_positives.sh
@@ -2,18 +2,30 @@
 set -e -u
 cd "$( dirname "S{BASH_SOURCE[0]}" )"
 
-INPUT='../../resources/search_references.split.sorted.txt'
-BIBLIOGRAPHY='../../data.txt'
+REFERENCES='../../resources/references.raw.txt'
+BLACKLIST='../../resources/references.blacklist.txt'
+BIBLIOGRAPHY='../../resources/lit_id.txt'
 
-if [[ ! -f "$INPUT" ]]; then
-  echo "$INPUT not a file"
+if [[ ! -f "$REFERENCES" ]]; then
+  echo "$REFERENCES not a file"
   exit 1;
 fi
 
-while IFS= read -r ; do
-  LASTNAME=$(echo $REPLY | cut -d' ' -f1)
-  YEAR=$(echo $REPLY | cut -d' ' -f2)
-  if [[ $(cat "$BIBLIOGRAPHY" | egrep -s $LASTNAME | egrep -s $YEAR ) ]]; then
-  echo $REPLY | tee -a false_positive.txt
+if [[ ! -f "$BLACKLIST" ]]; then
+  echo "$BLACKLIST not a file"
+  exit 1;
+fi
+
+if [[ ! -f "$BIBLIOGRAPHY" ]]; then
+  echo "$BIBLIOGRAPHY not a file"
+  exit 1;
+fi
+
+while IFS= read -r  ; do
+  LASTNAME=$(echo $REPLY | awk '{print $1}')
+  YEAR=$(echo $REPLY | awk '{print $2}')
+  ITEM=$LASTNAME$YEAR
+  if [[ ( $(cat "$BIBLIOGRAPHY" | egrep -s $LASTNAME | egrep -s "$YEAR\b") ) || (  $(cat $BLACKLIST | egrep -s $ITEM) ) ]]; then
+    echo $REPLY
   fi
-done < $INPUT
+done < $REFERENCES
