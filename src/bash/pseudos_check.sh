2908be965bedad180f07c3829cd6ea28f3d5ed0e
juan-amorocho <jamoroch@gmail.com>
2019-09-22 15:23:32 +0200

    pseudos check

---
+++ src/bash/pseudos_check.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e -u
+cd "$( dirname "S{BASH_SOURCE[0]}" )"
+
+TYPE=${1:-real}
+
+RESOURCES_DIR='../../resources'
+ORIGINAL_DIR=$RESOURCES_DIR/original
+NAMES_DIR=$RESOURCES_DIR/pseudos/$TYPE
+
+if [[ ! -d  "$NAMES_DIR" ]]; then
+  echo $NAMES_DIR unknown
+  exit 1;
+fi
+
+for FILE in $(ls -1 $NAMES_DIR);
+  do
+  echo $FILE
+  cat $FILE | xargs
+    ls -1 $ORIGINAL_DIR/K* | xargs -I {} grep --only-match --color='always' -f $NAMES_DIR/$FILE {}
+  done
