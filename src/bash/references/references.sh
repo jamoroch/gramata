107d54fc3ec43e3aeb05dd9bc259b77ebd9c1b75
juan-amorocho <jamoroch@gmail.com>
2019-11-02 17:01:58 +0100

    new data txt and its sorted version

--- src/bash/references/references.sh
+++ src/bash/references/references.sh
@@ -2,7 +2,6 @@
 set -e -u
 cd "$( dirname "S{BASH_SOURCE[0]}" )"
 FILE_DIR='../../../resources/original'
-OUTPUT_DIR='../../../resources/refs'
 if [[ ! -d "$FILE_DIR" ]]; then
   echo $FILE_DIR not a directory
   exit 1;
@@ -10,15 +9,16 @@ fi
 
 cd $FILE_DIR
 
-for i in $(seq 2 11); do
+for i in $(seq 1 11); do
   FILE=K$i.txt
   if [[ ! -f "$FILE" ]]; then
     echo "$FILE" not a FILE
     exit 1; 
   fi
   echo ${FILE%.txt}
-  cat $FILE | egrep -o '([A-Z]\w+\/\s?)?[A-Z]\w+ \(?[0-9]{4}[a-z]*' | sed -e 's/(//g' -e 's#/# #g' | sort | uniq
+  cat $FILE | egrep -o '([A-Z]\w+\/\s?)?[A-Z]\w+ \(?\d{4}[a-z]*(, \d{4}[a-z]*)*' | sed -e 's/(//g' -e 's#/# #g' | sort | uniq
   done
 
   cd -
   exit 0;
+  
