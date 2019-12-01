05d52ceabcfe0bd60864fd4f125339b27a361ba6
jamoroch <jamoroch@gmail.com>
2019-12-01 13:46:13 +0100

    add reverse reference referencee check

---
+++ src/bash/reverse_references_check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e -u
+cd "$( dirname "S{BASH_SOURCE[0]}" )"
+
+CHAPTERS_DIR='../../resources/original'
+DATA_FILENAME='../../resources/data.txt'
+
+cat $DATA_FILENAME | cut -d: -f1 | sed -e "s/[0-9]//g" -e "s/[()]//g" | \
+awk -F/ '{for(i=1;i<=NF;i++){split($i, a, ","); print a[1]}}' | sed -e "s/^ *//g" | sort | uniq
