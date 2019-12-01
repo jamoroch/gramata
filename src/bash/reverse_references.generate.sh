60a9a43ab531aefd35a913c94e32d4f52b56a6d8
juan-amorocho <jamoroch@gmail.com>
2019-12-01 18:40:55 +0100

    restrict reverse_reference algorithm to only first name

--- src/bash/reverse_references.generate.sh
+++ src/bash/reverse_references.generate.sh
@@ -6,4 +6,6 @@ CHAPTERS_DIR='../../resources/original'
 DATA_FILENAME='../../resources/data.txt'
 
 cat $DATA_FILENAME | cut -d: -f1 | sed -e "s/[0-9]//g" -e "s/[()]//g" | \
-awk -F/ '{for(i=1;i<=NF;i++){split($i, a, ","); print a[1]}}' | sed -e "s/^ *//g" | sort | uniq
+#awk -F/ '{for(i=1;i<=NF;i++){split($i, a, ","); print a[1]}}' | \
+awk -F/ '{ split($0, a, ","); print a[1] }' | \
+sed -e "s/^ *//g" | sort | uniq
