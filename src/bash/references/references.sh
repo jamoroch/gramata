3be520c21f0cd1f34cb45d1652c67250c8c8d4c3
juan-amorocho <jamoroch@gmail.com>
2019-12-28 21:11:00 +0100

    change references script to use pdfgrep

--- src/bash/references/references.sh
+++ src/bash/references/references.sh
@@ -2,23 +2,34 @@
 set -e -u
 cd "$( dirname "S{BASH_SOURCE[0]}" )"
 FILE_DIR='../../../resources/original'
+PDF_FILENAME='../../../resources/original/Kall.pdf'
+
+SED_CMD='/usr/local/Cellar/gnu-sed/4.5/bin/gsed'
+
 if [[ ! -d "$FILE_DIR" ]]; then
   echo $FILE_DIR not a directory
   exit 1;
 fi
 
-cd $FILE_DIR
+if [[ ! -f "$PDF_FILENAME" ]]; then
+  echo $PDF_FILENAME not found!
+  exit 1;
+fi
 
-for i in $(seq 1 11); do
-  FILE=K$i.txt
-  if [[ ! -f "$FILE" ]]; then
-    echo "$FILE" not a FILE
-    exit 1; 
-  fi
-  echo ${FILE%.txt}
-  cat $FILE | egrep -o '([A-Z]\w+\/\s?)?[A-Z]\w+\s?(et al.)? \(?\d{4}[a-z]*(, \d{4}[a-z]*)*' | sed -e 's/et al\.//g' -e 's/(//g' -e 's#/# #g' | sort | uniq
-  done
+#cd $FILE_DIR
 
-  cd -
+#for i in $(seq 1 11); do
+#  FILE=K$i.txt
+#  if [[ ! -f "$FILE" ]]; then
+#    echo "$FILE" not a FILE
+#    exit 1; 
+#  fi
+#  echo ${FILE%.txt}
+#  cat $FILE | egrep -o '([A-Z]\w+\/\s?)?[A-Z]\w+\s?(et al.)? \(?\d{4}[a-z]*(, \d{4}[a-z]*)*' | sed -e 's/et al\.//g' -e 's/(//g' -e 's#/# #g' | sort | uniq
+#  done
+
+pdfgrep -Po '([A-Z]\w+\/\s?)?[A-Z]\w+\s?(et al.)? \(?\d{4}[a-z]*(, \d{4}[a-z]*)*' $PDF_FILENAME | $SED_CMD -e 's/et al\.//g' -e 's/(//g' -e 's#/# #g' -e 's/^[ ]*//' -e 's/[ ]*$//' -e 's/\s\{2,\}/ /' | sort | uniq
+
+ # cd -
   exit 0;
   
--- src/bash/reverse_references.check.sh
+++ src/bash/reverse_references.check.sh
@@ -3,6 +3,17 @@ cd "$( dirname "S{BASH_SOURCE[0]}" )"
 KALL='../../resources/original/Kall.pdf'
 REVERSE_REFERENCE_FILENAME='../../resources/reverse_references.txt'
 
+if [[ ! -f $KALL ]]; then
+  echo $KALL not found!
+  exit 1
+fi
+
+if [[ ! -f $REVERSE_REFERENCE_FILENAME ]]; then
+  echo $REVERSE_REFERENCE_FILENAME not found!
+  exit 1
+fi
+
+
 while IFS= read -r  ; do
   WORD=$(echo $REPLY | sed -e "s/[ \t]$+//g")
   pdfgrep --quiet "$WORD" $KALL
